c53f8b1daa32dd69a2f74e1bbf8f228f93abd613
AMO-Project <ico@amo.foundation>
2018-04-12 17:12:22 +0900

    Remove needless function that returns whether sale is end or not

--- contracts/AMOCoinSale.sol
+++ contracts/AMOCoinSale.sol
@@ -389,10 +389,12 @@ contract AMOCoinSale is Pausable {
         return true;
     }
 
-    function isSaleEnded() public view returns (bool) {
-        return stage == Stages.Ended;
-    }
-
+    /*
+     * Add user and his allowed amount to allocation list
+     *
+     * @param user: Address of user to be allocated tokens
+     * @param amount: Allowed allocation amount of user
+     */
     function addToAllocationList(address user, uint256 amount)
         public
         onlyOwner
